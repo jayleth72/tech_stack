ca3cffff9133a72bb1ae1e63f1c35b767547deb0
Jay Letheby <jay.letheby@gmail.com>
2017-06-11 22:22:48 +1000

    ListView working

---
+++ src/components/common/Header.js
@@ -0,0 +1,35 @@
+// Import libraries for making a Component
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// Make a compenent
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+
+  );
+};
+
+const styles = {
+  viewStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    backgroundColor: '#F8F8F8',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 20
+  }
+};
+// Make the component available to other parts of the app
+export {Header};
